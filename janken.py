6a2a7e05595b2e0b5a83cd396b31b80a84444f34
eisuke <e.nakanishi.sys24@morijyobi.ac.jp>
2024-11-08 11:35:16 +0900

    じゃんけんコントロール

---
+++ janken.py
@@ -0,0 +1,9 @@
+you_count = 0
+for a in range(3):
+    print(f'----ラウンド{a+1}----')
+    n = 0
+    you = 0
+
+    print(f'\n相手の手 : {enemy} \nあなたの手 : {you}\nあなたの{prin}\n\n----------------')
+    
+print(f'あなたの勝利数は{you_count}です。')
